49bf7806aa0efba349b212e517f07a5834f36a43
Omri Steinberg <omri.steinberg@abra-it.com>
2024-06-14 04:20:04 +0300

    build: cors configuration

--- Composers/CorsComposer.cs
+++ Composers/CorsComposer.cs
@@ -1,16 +1,27 @@
 
 using Umbraco.Cms.Core.Composing;
 using Umbraco.Cms.Web.Common.ApplicationBuilder;
+using Umbraco13Test.Models;
 
 public class CorsComposer : IComposer
 {
     public const string AllowAnyOriginPolicyName = nameof(AllowAnyOriginPolicyName);
-
     public void Compose(IUmbracoBuilder builder)
     {
+        var allowedOrigins = builder.Config.GetSection(nameof(CorsSettings)).Get<CorsSettings>()?.AllowedOrigins;
+        if (allowedOrigins is null || allowedOrigins.Length == 0)
+        {
+            throw new Exception("AllowedOrigins must be set in the appsettings.json file");
+        }
+
         // Can also use AllowAnyOrigin method
         builder.Services
-       .AddCors(options => options.AddDefaultPolicy(policy => policy.WithOrigins("*").AllowAnyHeader().AllowAnyMethod()))
+       .AddCors(options => options.AddDefaultPolicy(policy =>
+            policy
+                .WithOrigins(allowedOrigins!)
+                .AllowAnyHeader()
+                .AllowAnyMethod()
+                .AllowCredentials()))
        .Configure<UmbracoPipelineOptions>(options =>
             options.AddFilter(new UmbracoPipelineFilter("Cors", postRouting: app => app.UseCors())));
     }
---
+++ Models/CorsSettings.cs
@@ -0,0 +1,6 @@
+namespace Umbraco13Test.Models;
+
+public class CorsSettings
+{
+    public string[] AllowedOrigins { get; set; } = new string[] { };
+}
